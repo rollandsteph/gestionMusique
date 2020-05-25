92096b5010c4e9bb24de7a1d6aef1e0fe0b16edf
rollandsteph <rolland.steph@gmail.com>
2020-05-26 00:25:45 +0200

    gestion des albums et artistes

---
+++ src/Controller/ArtisteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Artiste;
+use App\Repository\ArtisteRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArtisteController extends AbstractController
+{
+    /**
+     * @Route("/artistes", name="artistes", methods={"GET"})
+     */
+    public function listeArtistes(ArtisteRepository $repo)
+    {
+        $artistes=$repo->findAll();
+        return $this->render('artiste/listeArtistes.html.twig', [
+            'lesArtistes' => $artistes
+        ]);
+    }
+
+    /**
+     * @Route("/artiste/{id}", name="ficheArtiste", methods={"GET"})
+     */
+    public function ficheArtiste(Artiste $artiste)
+    {
+        return $this->render('artiste/ficheArtiste.html.twig', [
+            'leArtiste' => $artiste
+        ]);
+    }
+}
---
+++ src/Migrations/Version20200524233010.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200524233010 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE album (id INT NOT NULL, artiste_id INT NOT NULL, nom VARCHAR(255) NOT NULL, date INT NOT NULL, image VARCHAR(255) NOT NULL, INDEX IDX_39986E4321D25844 (artiste_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE artiste (id INT NOT NULL, nom VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, site VARCHAR(255) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE morceau (id INT NOT NULL, album_id INT NOT NULL, titre VARCHAR(255) NOT NULL, duree VARCHAR(255) NOT NULL, piste INT NOT NULL, INDEX IDX_36BB72081137ABCF (album_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE style (id INT NOT NULL, nom VARCHAR(255) NOT NULL, couleur VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE style_album (style_id INT NOT NULL, album_id INT NOT NULL, INDEX IDX_F34D20B8BACD6074 (style_id), INDEX IDX_F34D20B81137ABCF (album_id), PRIMARY KEY(style_id, album_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE album ADD CONSTRAINT FK_39986E4321D25844 FOREIGN KEY (artiste_id) REFERENCES artiste (id)');
+        $this->addSql('ALTER TABLE morceau ADD CONSTRAINT FK_36BB72081137ABCF FOREIGN KEY (album_id) REFERENCES album (id)');
+        $this->addSql('ALTER TABLE style_album ADD CONSTRAINT FK_F34D20B8BACD6074 FOREIGN KEY (style_id) REFERENCES style (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE style_album ADD CONSTRAINT FK_F34D20B81137ABCF FOREIGN KEY (album_id) REFERENCES album (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE morceau DROP FOREIGN KEY FK_36BB72081137ABCF');
+        $this->addSql('ALTER TABLE style_album DROP FOREIGN KEY FK_F34D20B81137ABCF');
+        $this->addSql('ALTER TABLE album DROP FOREIGN KEY FK_39986E4321D25844');
+        $this->addSql('ALTER TABLE style_album DROP FOREIGN KEY FK_F34D20B8BACD6074');
+        $this->addSql('DROP TABLE album');
+        $this->addSql('DROP TABLE artiste');
+        $this->addSql('DROP TABLE morceau');
+        $this->addSql('DROP TABLE style');
+        $this->addSql('DROP TABLE style_album');
+    }
+}
